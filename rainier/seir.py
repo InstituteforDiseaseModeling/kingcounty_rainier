7d28c38bbcc616622bd3b1f8dfd33e22a4297c45
Mary Fisher <mafisher@idmod.org>
2020-05-22 09:12:29 -0700

    add synthetic data set and PEP8 changes

--- rainier/seir.py
+++ rainier/seir.py
@@ -17,135 +17,133 @@ from scipy.optimize import minimize
 ## For good hygiene
 import warnings
 
+
 class LogNormalSEIR(object):
+    """ Discrete time SEIR model with log-normally distributed transmission.
 
-	""" Discrete time SEIR model with log-normally distributed transmission. 
+    S_t = S_{t-1} - E_{t-1}
+    E_t = beta*S_{t-1}*(I_{t-1}+z_{t-1})*epsilon_t + (1-(1/D_e))*E_{t-1}
+    I_t = (1/D_e)*E{t-1} + (1-(1/D_i))*I_{t-1}
+    C_t ~ Binomial(I_t,p)
 
-	S_t = S_{t-1} - E_{t-1}
-	E_t = beta*S_{t-1}*(I_{t-1}+z_{t-1})*epsilon_t + (1-(1/D_e))*E_{t-1}
-	I_t = (1/D_e)*E{t-1} + (1-(1/D_i))*I_{t-1}
-	C_t ~ Binomial(I_t,p) 
+    D_e, D_i, z_t, and the initial condition are assumed known. beta, alpha, p, and
+    transmission variance are meant to be inferred from data. """
 
-	D_e, D_i, z_t, and the initial condition are assumed known. beta, alpha, p, and 
-	transmission variance are meant to be inferred from data. """
+    def __init__(self, S0, D_e, D_i, z_t):
 
-	def __init__(self,S0,D_e,D_i,z_t):
+        ## Store the known model parameters
+        self.z_t = z_t
+        self.S0 = S0
+        self.D_e = D_e
+        self.D_i = D_i
 
-		## Store the known model parameters
-		self.z_t = z_t
-		self.S0 = S0
-		self.D_e = D_e
-		self.D_i = D_i
+        ## Create a time axis
+        self.T = len(z_t)
+        self.time = np.arange(self.T)
 
-		## Create a time axis
-		self.T = len(z_t)
-		self.time = np.arange(self.T)
+        ## Mark the model as un-fit, which means parameters
+        ## are missing.
+        self._fit = False
 
-		## Mark the model as un-fit, which means parameters
-		## are missing.
-		self._fit = False
+    def sample(self, beta_t, num_samples=10000, sig_eps=None, z_t=None):
 
-	def sample(self,beta_t,num_samples=10000,sig_eps=None,z_t=None):
+        """ Sample the model, num_samples is the number of sample trajectories. Output has shape
+        (num_samples,4,len(beta_t)). """
 
-		""" Sample the model, num_samples is the number of sample trajectories. Output has shape
-		(num_samples,4,len(beta_t)). """
+        ## Set the variance for these samples
+        if sig_eps is None:
+            sig_eps = self.sig_eps * np.ones((len(beta_t),))
 
-		## Set the variance for these samples
-		if sig_eps is None:
-			sig_eps = self.sig_eps*np.ones((len(beta_t),))
+        ## Set the importation scenario
+        if z_t is None:
+            z_t = self.z_t
 
-		## Set the importation scenario
-		if z_t is None:
-			z_t = self.z_t
+        ## Allocate storage for the output, and set up the
+        ## initial condition.
+        X = np.zeros((num_samples, 3, len(beta_t)))
+        X[:, 0, 0] = self.S0
 
-		## Allocate storage for the output, and set up the
-		## initial condition.
-		X = np.zeros((num_samples,3,len(beta_t)))
-		X[:,0,0] = self.S0
+        ## Loop over time and collect samples
+        for t in range(1, len(beta_t)):
+            ## Sample eps_t
+            eps_t = np.exp(np.random.normal(0, sig_eps[t], size=(num_samples,)))
 
-		## Loop over time and collect samples
-		for t in range(1,len(beta_t)):
+            ## Update all the deterministic components (S and I)
+            X[:, 0, t] = X[:, 0, t - 1] - beta_t[t] * X[:, 0, t - 1] * (X[:, 2, t - 1] + z_t[t - 1]) * eps_t
+            X[:, 2, t] = X[:, 1, t - 1] / self.D_e + X[:, 2, t - 1] * (1. - (1. / self.D_i))
 
-			## Sample eps_t
-			eps_t = np.exp(np.random.normal(0,sig_eps[t],size=(num_samples,)))
+            ## Update the exposed compartment accross samples
+            X[:, 1, t] = beta_t[t] * X[:, 0, t - 1] * (X[:, 2, t - 1] + z_t[t - 1]) * eps_t + \
+                         X[:, 1, t - 1] * (1. - (1. / self.D_e))
 
-			## Update all the deterministic components (S and I)
-			X[:,0,t] = X[:,0,t-1]-beta_t[t]*X[:,0,t-1]*(X[:,2,t-1]+z_t[t-1])*eps_t
-			X[:,2,t] = X[:,1,t-1]/self.D_e + X[:,2,t-1]*(1.-(1./self.D_i))
+            ## High sig-eps models require by-hand enforcement
+            ## of positivity (i.e. truncated gaussians).
+            X[X[:, 2, t] < 0, 2, t] = 0
+            X[X[:, 1, t] < 0, 1, t] = 0
+            X[X[:, 0, t] < 0, 0, t] = 0
 
-			## Update the exposed compartment accross samples
-			X[:,1,t] = beta_t[t]*X[:,0,t-1]*(X[:,2,t-1]+z_t[t-1])*eps_t+\
-					   X[:,1,t-1]*(1.-(1./self.D_e))
+        return X
 
-			## High sig-eps models require by-hand enforcement
-			## of positivity (i.e. truncated gaussians).
-			X[X[:,2,t]<0,2,t] = 0
-			X[X[:,1,t]<0,1,t] = 0
-			X[X[:,0,t]<0,0,t] = 0
+    def mean(self, beta_t, z_t=None, sig_eps=None):
 
-		return X
+        """ Compute the mean trajectory given a time-varying beta_t series. """
 
-	def mean(self,beta_t,z_t=None,sig_eps=None):
+        ## Set the importation scenario
+        if z_t is None:
+            z_t = self.z_t
 
-		""" Compute the mean trajectory given a time-varying beta_t series. """
+        ## Set the variance over time
+        if sig_eps is None:
+            sig_eps = self.sig_eps * np.ones((len(beta_t),))
 
-		## Set the importation scenario
-		if z_t is None:
-			z_t = self.z_t
+        ## Allocate storage for the output, and set up the
+        ## initial condition.
+        X = np.zeros((3, len(beta_t)))
+        X[0, 0] = self.S0
 
-		## Set the variance over time
-		if sig_eps is None:
-			sig_eps = self.sig_eps*np.ones((len(beta_t),))
+        ## Loop over time and collect samples
+        eps_t = np.exp(0.5 * sig_eps ** 2)
+        for t in range(1, len(beta_t)):
+            ## Update all the deterministic components (all of them in this case)
+            X[0, t] = X[0, t - 1] - beta_t[t] * X[0, t - 1] * (X[2, t - 1] + z_t[t - 1]) * eps_t[t]
+            X[2, t] = X[1, t - 1] / self.D_e + X[2, t - 1] * (1. - (1. / self.D_i))
+            X[1, t] = beta_t[t] * X[0, t - 1] * (X[2, t - 1] + z_t[t - 1]) * eps_t[t] + \
+                      X[1, t - 1] * (1. - (1. / self.D_e))
 
-		## Allocate storage for the output, and set up the
-		## initial condition.
-		X = np.zeros((3,len(beta_t)))
-		X[0,0] = self.S0
+        return X
 
-		## Loop over time and collect samples
-		eps_t = np.exp(0.5*sig_eps**2)
-		for t in range(1,len(beta_t)):
 
-			## Update all the deterministic components (all of them in this case)
-			X[0,t] = X[0,t-1]-beta_t[t]*X[0,t-1]*(X[2,t-1]+z_t[t-1])*eps_t[t]
-			X[2,t] = X[1,t-1]/self.D_e + X[2,t-1]*(1.-(1./self.D_i))
-			X[1,t] = beta_t[t]*X[0,t-1]*(X[2,t-1]+z_t[t-1])*eps_t[t]+\
-					   X[1,t-1]*(1.-(1./self.D_e))
+def SampleMortality(model, samples, ifr_samples, delay_samples):
+    """ Sample mortality accross samples of ifr and delays. """
 
-		return X
+    ## Calculate new exposures
+    new_exposures = samples[:, 1, 1:] - (1. - (1. / model.D_e)) * samples[:, 1, :-1]
 
-def SampleMortality(model,samples,ifr_samples,delay_samples):
+    ## Compute destined deaths with a different IFR for each
+    ## trajectory.
+    destined_deaths = np.random.binomial(np.round(new_exposures).astype(int),
+                                         p=ifr_samples[:, np.newaxis])
 
-	""" Sample mortality accross samples of ifr and delays. """
+    ## Finally, use np.roll to shift by appropriate numbers, and then
+    ## zero rolled entries for daily deaths (this is the slow part!)
+    daily_deaths = advindexing_roll(destined_deaths, delay_samples)
+    for i, d in enumerate(delay_samples):
+        daily_deaths[i, :d] = 0
 
-	## Calculate new exposures
-	new_exposures = samples[:,1,1:] - (1. - (1./model.D_e))*samples[:,1,:-1]
+    return destined_deaths, daily_deaths
 
-	## Compute destined deaths with a different IFR for each
-	## trajectory.
-	destined_deaths = np.random.binomial(np.round(new_exposures).astype(int),
-										 p=ifr_samples[:,np.newaxis])
-
-	## Finally, use np.roll to shift by appropriate numbers, and then
-	## zero rolled entries for daily deaths (this is the slow part!)
-	daily_deaths = advindexing_roll(destined_deaths,delay_samples)
-	for i,d in enumerate(delay_samples):
-		daily_deaths[i,:d] = 0
-
-	return destined_deaths, daily_deaths
 
 def advindexing_roll(A, r):
-	
-	""" The following code was originally posted to StackOverflow as part of 
-	question https://stackoverflow.com/questions/20360675/roll-rows-of-a-matrix-independently. 
-	Contributing authors were seberg (https://stackoverflow.com/users/455221/seberg), 
-	yatu (https://stackoverflow.com/users/9698684/yatu), 
-	divakar (https://stackoverflow.com/users/3293881/divakar), 
-	Yann Dubois (https://stackoverflow.com/users/6232494/yann-dubois), and 
-	logicOnAbstractions (https://stackoverflow.com/users/3633696/logiconabstractions). 
-	Code from StackOverflow is provided subject to the Creative Commons Attribution Share-Alike license. """
-	
-	rows, column_indices = np.ogrid[:A.shape[0], :A.shape[1]]
-	r[r < 0] += A.shape[1]
-	column_indices = column_indices - r[:,np.newaxis]
-	return A[rows, column_indices]
+    """ The following code was originally posted to StackOverflow as part of
+    question https://stackoverflow.com/questions/20360675/roll-rows-of-a-matrix-independently.
+    Contributing authors were seberg (https://stackoverflow.com/users/455221/seberg),
+    yatu (https://stackoverflow.com/users/9698684/yatu),
+    divakar (https://stackoverflow.com/users/3293881/divakar),
+    Yann Dubois (https://stackoverflow.com/users/6232494/yann-dubois), and
+    logicOnAbstractions (https://stackoverflow.com/users/3633696/logiconabstractions).
+    Code from StackOverflow is provided subject to the Creative Commons Attribution Share-Alike license. """
+
+    rows, column_indices = np.ogrid[:A.shape[0], :A.shape[1]]
+    r[r < 0] += A.shape[1]
+    column_indices = column_indices - r[:, np.newaxis]
+    return A[rows, column_indices]
--- report_april26/ModelCompare.py
+++ report_april26/ModelCompare.py
@@ -4,6 +4,7 @@ Compare the previous KC reports' statistical model output to the new version
 based on updated data. """
 import sys
 from pathlib import Path
+
 sys.path.append(Path("../"))
 import warnings
 
@@ -11,56 +12,62 @@ import warnings
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 np.random.seed(6)
 
 ## For plot settings
 import rainier
 
+
 def axes_setup(axes):
-	axes.spines["left"].set_position(("axes",-0.025))
-	axes.spines["top"].set_visible(False)
-	axes.spines["right"].set_visible(False)
-	return
+    axes.spines["left"].set_position(("axes", -0.025))
+    axes.spines["top"].set_visible(False)
+    axes.spines["right"].set_visible(False)
+    return
+
 
 if __name__ == "__main__":
+    ## Get the second report
+    kc2_points = pd.read_pickle(Path("../_outputs/r0_4_10.pkl"))
+    kc2_m = pd.read_pickle(Path("../_outputs/mobility_r0_4_10.pkl"))
 
-	## Get the second report
-	kc2_points = pd.read_pickle(Path("../_outputs/r0_4_10.pkl"))
-	kc2_m = pd.read_pickle(Path("../_outputs/mobility_r0_4_10.pkl"))
+    ## Get the third report
+    kc3_points = pd.read_pickle(Path("../_outputs/r0_4_19.pkl"))
 
-	## Get the third report
-	kc3_points = pd.read_pickle(Path("../_outputs/r0_4_19.pkl"))
+    ## Get the latest points
+    kc4_points = pd.read_pickle(Path("../_outputs/r0_4_26.pkl"))
 
-	## Get the latest points
-	kc4_points = pd.read_pickle(Path("../_outputs/r0_4_26.pkl"))
+    ## Set up a figure
+    fig, axes = plt.subplots(figsize=(18, 9), sharex=True)
+    axes_setup(axes)
 
-	## Set up a figure
-	fig, axes = plt.subplots(figsize=(18,9),sharex=True)
-	axes_setup(axes)
+    ## Plot the mobility model
+    fit = kc2_m.loc[:"2020-03-25"]
+    forecast = kc2_m.loc["2020-03-26":]
+    axes.plot(forecast.index, forecast["low"], "#FAAF08", lw=3, ls="dashed")
+    axes.plot(forecast.index, forecast["high"], "#FAAF08", lw=3, ls="dashed",
+              label="Mobility-based nowcast from the 4/10 report")
 
-	## Plot the mobility model
-	fit = kc2_m.loc[:"2020-03-25"]
-	forecast = kc2_m.loc["2020-03-26":]
-	axes.plot(forecast.index,forecast["low"],"#FAAF08",lw=3,ls="dashed")
-	axes.plot(forecast.index,forecast["high"],"#FAAF08",lw=3,ls="dashed",label="Mobility-based nowcast from the 4/10 report")
-	
-	## Plot the data
-	shift = pd.to_timedelta(0.1,unit="d")
-	axes.errorbar(kc2_points.index-shift,kc2_points["r0_t"].values,yerr=2.*kc2_points["std_err"],
-				  color="0.8",ls="None",marker="o",label=r"R$_e$ estimates based on WDRS compiled on 4/3 (4/10 report)")
-	axes.errorbar(kc3_points.index,kc3_points["r0_t"].values,yerr=2.*kc3_points["std_err"],
-				  color="0.5",ls="None",marker="o",label=r"R$_e$ estimates based on WDRS compiled on 4/19 (4/21 report)")
-	axes.errorbar(kc4_points.index+shift,kc4_points["r0_t"].values,yerr=2.*kc4_points["std_err"],
-				  color="k",ls="None",marker="o",label=r"R$_e$ estimates based on WDRS compiled on 4/26 (this report)")
+    ## Plot the data
+    shift = pd.to_timedelta(0.1, unit="d")
+    axes.errorbar(kc2_points.index - shift, kc2_points["r0_t"].values, yerr=2. * kc2_points["std_err"],
+                  color="0.8", ls="None", marker="o",
+                  label=r"R$_e$ estimates based on WDRS compiled on 4/3 (4/10 report)")
+    axes.errorbar(kc3_points.index, kc3_points["r0_t"].values, yerr=2. * kc3_points["std_err"],
+                  color="0.5", ls="None", marker="o",
+                  label=r"R$_e$ estimates based on WDRS compiled on 4/19 (4/21 report)")
+    axes.errorbar(kc4_points.index + shift, kc4_points["r0_t"].values, yerr=2. * kc4_points["std_err"],
+                  color="k", ls="None", marker="o",
+                  label=r"R$_e$ estimates based on WDRS compiled on 4/26 (this report)")
 
-	## Re = 1 line?
-	axes.set_ylim((0,5))
-	axis_to_data = axes.transAxes + axes.transData.inverted()
-	axes.axhline(1.0,c="#4D648D",lw=2,ls="dashed",label=r"Threshold for declining transmission, R$_{e}=1$")
-	
-	## Details
-	axes.legend(loc=1,frameon=False,fontsize=28)
-	axes.set_ylabel(r"Effective reproductive number (R$_{e}$)")
-	fig.tight_layout()
-	fig.savefig(Path("../_plots/model_compare.png"))
-	plt.show()
+    ## Re = 1 line?
+    axes.set_ylim((0, 5))
+    axis_to_data = axes.transAxes + axes.transData.inverted()
+    axes.axhline(1.0, c="#4D648D", lw=2, ls="dashed", label=r"Threshold for declining transmission, R$_{e}=1$")
+
+    ## Details
+    axes.legend(loc=1, frameon=False, fontsize=28)
+    axes.set_ylabel(r"Effective reproductive number (R$_{e}$)")
+    fig.tight_layout()
+    fig.savefig(Path("../_plots/model_compare.png"))
+    plt.show()
